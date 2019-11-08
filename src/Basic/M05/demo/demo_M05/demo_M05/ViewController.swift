5dee5d3d5c40c11fa570e898a2c3b9d0cb08b2a7
Xin Guo <emagrorrimxguo@gmail.com>
2019-11-08 18:24:26 +0800

    M05 WIP

--- src/Basic/M05/demo/demo_M05/demo_M05/ViewController.swift
+++ src/Basic/M05/demo/demo_M05/demo_M05/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var containerView: UIView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var contentLabel: UIView!
+  @IBOutlet weak var imageContentView: UIView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
