616ade00ec554ee02eddfd6501e4c90a0fae44e7
weicheng <809405366@qq.com>
2019-12-30 14:35:03 +0800

    add Table view cell

---
+++ src/Basic/project/MiniWeibo/MiniWeibo/Classes/View/WeiboTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeiboTableViewCell.swift
+//  MiniWeibo
+//
+//  Created by Weicheng Wang on 2019/12/30.
+//  Copyright © 2019 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class WeiboTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
