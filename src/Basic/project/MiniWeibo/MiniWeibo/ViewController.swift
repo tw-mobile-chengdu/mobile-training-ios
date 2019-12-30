344b5ed13c3e667bf01a78f5fd115d6b9f0df5a9
weicheng <809405366@qq.com>
2019-12-30 15:46:33 +0800

    add tableview deletage in ViewController

--- src/Basic/project/MiniWeibo/MiniWeibo/ViewController.swift
+++ src/Basic/project/MiniWeibo/MiniWeibo/ViewController.swift
@@ -15,8 +15,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "WeiboTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "WeiboTableViewCell")
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeiboTableViewCell", for: indexPath)
+        // TODO: bind data for cell.
+        return cell
+    }
+}
