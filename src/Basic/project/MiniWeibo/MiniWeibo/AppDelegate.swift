0225d810ecca84b231cf363231ceed404f893d4c
weicheng <809405366@qq.com>
2020-01-03 14:56:55 +0800

    remove constraint log

--- src/Basic/project/MiniWeibo/MiniWeibo/AppDelegate.swift
+++ src/Basic/project/MiniWeibo/MiniWeibo/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // 屏蔽控制台autolayout约束内容的输出
+        UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
         return true
     }
 
